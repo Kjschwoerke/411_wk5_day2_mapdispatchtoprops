500adfde6d673514d099116e2abaff60601e21e6
kjschwoerke <kjschwoerke@yahoo.com>
2020-02-10 20:21:26 -0600

    delete function working

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -30,8 +30,10 @@ class AddCar extends Component {
         payload.id = this.props.carTotal + 1
         delete payload.open
         console.log("THE CAR", payload)
+        this.props.addCar(payload)
         // add this.props.addCar function here
         // also add this.setState to close the dialog
+        this.setState({open: false})
     }
 
     componentDidUpdate = (prevProps, prevState) => {
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,19 @@
 import { connect } from 'react-redux'
 import Home from '../components/Home'
+import { addCar, removeCar } from '../redux/actions'
 
-const mapStateToProps = (state) => {
-    return {
-        cars: state.cars
+    const mapStateToProps = (state) => {
+        return {
+            cars: state.cars
+        }
     }
-}
 
-export default connect(mapStateToProps)(Home)
+    // added this function so we can send data FROM our component
+    const mapDispatchToProps = (dispatch) => {
+        return {
+            addCar: (car) => dispatch(addCar(car)),
+            removeCar: (index) => dispatch(removeCar(index))
+        }
+    }
+
+    export default connect(mapStateToProps, mapDispatchToProps)(Home)
